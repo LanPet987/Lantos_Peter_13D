73a8aa1ad4ddaefca82bff79ad263eff4be1d43a
LanPet987 <115153511+LanPet987@users.noreply.github.com>
2025-09-11 13:22:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,35 @@ app.get('/', (req, res) => {
   res.send('Fut a backend');
 });
 
+app.get('/regiok', (req, res) => {
+  const sql = "SELECT * FROM `regiok`";
+  db.query(sql, (err, result) => {
+    if (err) { return res.json(err); }
+    return res.json(result);
+  })
+});
+
+app.post("/ujregio", (req, res) => {
+  const sql = "insert into `regiok`(`Rid`, `regionev`, `regio_tipusa`) VALUES (?,?,?) ";
+  const values = ['11', 'Budapest', 'Főváros']
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("hiba történt:", err);
+      return res.status(500).json({ error: "Adatbázis hiba történt" });
+    }
+    return res.status(200).json({ message: "Sikeres beszúrás", result });
+
+  })
+});
+
+app.delete("/torles/:id", (req, res) => {
+  const sql = "DELETE FROM `regiok` WHERE Rid = ?";
+  db.query(sql, [req.params.id], (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  })
+});
+
 app.listen(port, () => {
   console.log("A szerver fut a 3000 porton.");
 });

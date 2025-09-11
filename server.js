400297f657e2c3220ce923e89397f9c27abbc4db
LanPet987 <115153511+LanPet987@users.noreply.github.com>
2025-09-11 12:22:26 +0200

    új get és post hozzáadva

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
 
 app.use(express.json());
 
 const cors = require('cors');
 app.use(cors());
 
-
+const db = mysql.createConnection({
+  user: 'root',
+  host: '127.0.0.1',
+  port: '3307',
+  password: '',
+  database: 'kozutak'
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Fut a backend');
 });
 
 app.listen(port, () => {
